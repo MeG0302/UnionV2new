e127c13a93eb22a25a80f31c09cf3feb3d558061
MeG <112762788+MeG0302@users.noreply.github.com>
2025-05-10 03:47:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-"require('dotenv').config();
+require('dotenv').config();
 const { ethers } = require("ethers");
 const fs = require("fs");
 const usdcAbi = require("./abis/usdc.json");
@@ -10,45 +10,35 @@ const SEPOLIA_BRIDGE_ADDRESS = "0x5FbE74A283f7954f10AA04C2eDf55578811aeb03";
 const sepoliaProvider = new ethers.providers.JsonRpcProvider("https://ethereum-sepolia.publicnode.com");
 
 async function bridgeUSDC(wallet, maxTransaction = 1) {
-  try {
-    const address = await wallet.getAddress();
-    const usdc = new ethers.Contract(SEPOLIA_USDC_ADDRESS, usdcAbi, wallet);
-    const bridge = new ethers.Contract(SEPOLIA_BRIDGE_ADDRESS, bridgeAbi, wallet);
+  const address = await wallet.getAddress();
+  const usdc = new ethers.Contract(SEPOLIA_USDC_ADDRESS, usdcAbi, wallet);
+  const bridge = new ethers.Contract(SEPOLIA_BRIDGE_ADDRESS, bridgeAbi, wallet);
 
-    const decimals = await usdc.decimals();
-    const balance = await usdc.balanceOf(address);
-    if (balance.eq(0)) {
-      console.log(`No USDC balance for ${address}`);
-      return;
-    }
-
-    const babyWallets = JSON.parse(fs.readFileSync("./babylon-wallets.json", "utf8"));
-    const babyAddress = babyWallets[address.toLowerCase()];
-
-    if (!babyAddress) {
-      console.log(`No Babylon address mapped for ${address}`);
-      return;
-    }
-
-    const amountToBridge = balance.gt(ethers.utils.parseUnits(maxTransaction.toString(), decimals))
-      ? ethers.utils.parseUnits(maxTransaction.toString(), decimals)
-      : balance;
-
-    const allowance = await usdc.allowance(address, SEPOLIA_BRIDGE_ADDRESS);
-    if (allowance.lt(amountToBridge)) {
-      const approveTx = await usdc.approve(SEPOLIA_BRIDGE_ADDRESS, amountToBridge);
-      const approveReceipt = await approveTx.wait();
-      console.log(`Approved ${ethers.utils.formatUnits(amountToBridge, decimals)} USDC for bridging from ${address}`);
-      console.log(`Gas used for approval: ${approveReceipt.gasUsed.toString()}`);
-    }
-
-    const transferTx = await bridge.transferToBabylon(SEPOLIA_USDC_ADDRESS, amountToBridge, babyAddress);
-    const transferReceipt = await transferTx.wait();
-    console.log(`Bridged ${ethers.utils.formatUnits(amountToBridge, decimals)} USDC from ${address} to ${babyAddress} on Babylon.`);
-    console.log(`Gas used for transfer: ${transferReceipt.gasUsed.toString()}`);
-  } catch (err) {
-    console.error("Error in bridgeUSDC:", err);
+  const decimals = await usdc.decimals();
+  const balance = await usdc.balanceOf(address);
+  if (balance.eq(0)) {
+    console.log(`No USDC balance for ${address}`);
+    return;
   }
+
+  const babyWallets = JSON.parse(fs.readFileSync("./babylon-wallets.json", "utf8"));
+  const babyAddress = babyWallets[address.toLowerCase()];
+
+  if (!babyAddress) {
+    console.log(`No Babylon address mapped for ${address}`);
+    return;
+  }
+
+  const allowance = await usdc.allowance(address, SEPOLIA_BRIDGE_ADDRESS);
+  if (allowance.lt(balance)) {
+    const approveTx = await usdc.approve(SEPOLIA_BRIDGE_ADDRESS, balance);
+    await approveTx.wait();
+    console.log(`Approved USDC for bridging from ${address}`);
+  }
+
+  const transferTx = await bridge.transferToBabylon(SEPOLIA_USDC_ADDRESS, balance, babyAddress);
+  await transferTx.wait();
+  console.log(`Bridged ${ethers.utils.formatUnits(balance, decimals)} USDC from ${address} to ${babyAddress} on Babylon.`);
 }
 
 module.exports = {
